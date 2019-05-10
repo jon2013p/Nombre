c444748058184094b57654b475ffbf1a9e896522
jon2013p <49683620+jon2013p@users.noreply.github.com>
2019-05-10 09:01:53 -0500

    Add files via upload

---
+++ letraJ.py
@@ -0,0 +1,23 @@
+import turtle
+t=turtle
+t=turtle.Pen()
+
+
+t.right(90)
+t.forward(50)
+t.left(90)
+t.forward(100)
+t.left(90)
+t.forward(150)
+t.left(90)
+t.forward(70)
+t.left(180)
+t.forward(140)
+
+
+
+
+
+
+
+		
